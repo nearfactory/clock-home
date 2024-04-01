15de69d9812947179281608b18feb1cf13528ba8
Yuji-Terada <yuji070902@gmail.com>
2024-04-01 23:53:50 +0900

    カレンダー2種類に増やした

--- asset/script.js
+++ asset/script.js
@@ -1,5 +1,5 @@
-const day = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-const month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
 function updateTime(){
   // 日付オブジェクトを取得
@@ -7,7 +7,7 @@ function updateTime(){
 
   document.getElementById("time").textContent = String(now.getHours()).padStart(2, '0') + ":" +  String(now.getMinutes()).padStart(2, '0');
 
-  document.getElementById("dayAndMonth").textContent = day[now.getDay()%7] + " " + month[now.getMonth()%12];
+  document.getElementById("dayAndMonth").textContent = days[now.getDay()%7] + " " + months[now.getMonth()%12];
   document.getElementById("date").textContent = now.getDate();
 
   const second = now.getSeconds();
@@ -30,6 +30,46 @@ function updateTime(){
     document.documentElement.style.setProperty('--span1', "#e0e0e0");
     document.documentElement.style.setProperty('--span2', "#f2f2f2");
   }
+
+
+  const weeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+  const date = new Date()
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const startDate = new Date(year, month - 1, 1) // 月の最初の日を取得
+  const endDate = new Date(year, month,  0) // 月の最後の日を取得
+  const endDayCount = endDate.getDate() // 月の末日
+  const startDay = startDate.getDay() // 月の最初の日の曜日を取得
+  let dayCount = 1 // 日にちのカウント
+  let calendarHtml = '' // HTMLを組み立てる変数
+
+  calendarHtml += '<table>'
+
+  // 曜日の行を作成
+  for (let i = 0; i < weeks.length; i++) {
+      calendarHtml += '<td>' + weeks[i] + '</td>'
+  }
+
+  for (let w = 0; w < 6; w++) {
+      calendarHtml += '<tr>'
+
+      for (let d = 0; d < 7; d++) {
+          if (w == 0 && d < startDay) {
+              // 1行目で1日の曜日の前
+              calendarHtml += '<td></td>'
+          } else if (dayCount > endDayCount) {
+              // 末尾の日数を超えた
+              calendarHtml += '<td></td>'
+          } else {
+              calendarHtml += '<td>' + dayCount + '</td>'
+              dayCount++
+          }
+      }
+      calendarHtml += '</tr>'
+  }
+  calendarHtml += '</table>'
+
+  document.querySelector('#calendar2').innerHTML = calendarHtml
 }
 
 setInterval(updateTime, 10);
