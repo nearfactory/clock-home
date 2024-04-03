14574ec37c78d5dac94618b5e0b9cde9e8bca6b0
Yuji-Terada <yuji070902@gmail.com>
2024-04-03 21:24:08 +0900

    カレンダー2の今日の日付を○で囲った

--- asset/script.js
+++ asset/script.js
@@ -32,44 +32,47 @@ function updateTime(){
   }
 
 
-  const weeks = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-  const date = new Date()
-  const year = date.getFullYear()
-  const month = date.getMonth() + 1
-  const startDate = new Date(year, month - 1, 1) // 月の最初の日を取得
-  const endDate = new Date(year, month,  0) // 月の最後の日を取得
-  const endDayCount = endDate.getDate() // 月の末日
-  const startDay = startDate.getDay() // 月の最初の日の曜日を取得
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const startDate = new Date(year, month - 1, 1); // 月の最初の日を取得
+  const endDate = new Date(year, month,  0); // 月の最後の日を取得
+  const endDayCount = endDate.getDate(); // 月の末日
+  const startDay = startDate.getDay(); // 月の最初の日の曜日を取得
   let dayCount = 1 // 日にちのカウント
-  let calendarHtml = '' // HTMLを組み立てる変数
+  let calendarHtml = ''; // HTMLを組み立てる変数
 
-  calendarHtml += '<table>'
+  calendarHtml += '<table>';
 
   // 曜日の行を作成
-  for (let i = 0; i < weeks.length; i++) {
-      calendarHtml += '<td>' + weeks[i] + '</td>'
+  for (let i = 0; i < days.length; i++) {
+      calendarHtml += '<td>' + days[i] + '</td>';
   }
 
   for (let w = 0; w < 6; w++) {
-      calendarHtml += '<tr>'
+      calendarHtml += '<tr>';
 
       for (let d = 0; d < 7; d++) {
           if (w == 0 && d < startDay) {
               // 1行目で1日の曜日の前
-              calendarHtml += '<td></td>'
+              calendarHtml += '<td></td>';
           } else if (dayCount > endDayCount) {
               // 末尾の日数を超えた
-              calendarHtml += '<td></td>'
+              calendarHtml += '<td></td>';
           } else {
-              calendarHtml += '<td>' + dayCount + '</td>'
-              dayCount++
+              if(dayCount == now.getDate()){
+                calendarHtml += '<td class="today">' + dayCount + '</td>';
+              }
+              else{
+                calendarHtml += '<td>' + dayCount + '</td>';
+              }
+              dayCount++;
           }
       }
-      calendarHtml += '</tr>'
+      calendarHtml += '</tr>';
   }
-  calendarHtml += '</table>'
+  calendarHtml += '</table>';
 
-  document.querySelector('#calendar2').innerHTML = calendarHtml
+  document.querySelector('#calendar2').innerHTML = calendarHtml;
 }
 
 setInterval(updateTime, 10);
